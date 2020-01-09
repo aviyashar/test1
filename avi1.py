7a75cd8628c61f68b2a16edf6a2c2053a0f5f3dd
aviyashar <avi131293@gmail.com>
2020-01-09 16:46:52 +0200

    no message

--- avi1.py
+++ avi1.py
@@ -1,9 +1,3 @@
 print ("hello world11111")
-print ("hello wwedeqw")
-print ("hello wwedeqw")
-print ("hello wwedeqw")
-print ("hello wwedeqw")
-
-print ("hello wwedeqw")
-print ("hello wwedeqw")
+print ("hello aviiiiiiiiiii")
 
